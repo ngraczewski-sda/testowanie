bc65de349b52c8ace344577a1d0f18bfa2683498
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2023-03-02 23:02:26 +0100

    feat(004): update solution

--- jest/004-parzyste-nieparzyste/math.test.js
+++ jest/004-parzyste-nieparzyste/math.test.js
@@ -1,14 +1,14 @@
-const { isEven, isOdd, isDivisorOf } = require("./math");
+import { isDivisorOf, isEven, isOdd } from "./math";
 
 describe("math", () => {
   describe("isEven", () => {
-    it("should return true for even number", () => {
+    it("should return true for even numbers", () => {
       const result = isEven(2);
 
       expect(result).toBe(true);
     });
 
-    it("should return false for odd number", () => {
+    it("should return false for odd numbers", () => {
       const result = isEven(1);
 
       expect(result).toBe(false);
@@ -16,13 +16,13 @@ describe("math", () => {
   });
 
   describe("isOdd", () => {
-    it("should return true for odd number", () => {
+    it("should return true for odd numbers", () => {
       const result = isOdd(1);
 
       expect(result).toBe(true);
     });
 
-    it("should return false for even number", () => {
+    it("should return false for even numbers", () => {
       const result = isOdd(2);
 
       expect(result).toBe(false);
@@ -30,16 +30,30 @@ describe("math", () => {
   });
 
   describe("isDivisorOf", () => {
-    it("should return true for number b dividable by a", () => {
+    it("should return true if a is divisor of b", () => {
       const result = isDivisorOf(3, 27);
 
       expect(result).toBe(true);
     });
 
-    it("should return false for number b not dividable by a", () => {
-      const result = isDivisorOf(4, 27);
+    it("should return false if a is not a divisor of b", () => {
+      const result = isDivisorOf(4, 25);
 
       expect(result).toBe(false);
     });
   });
+});
+
+it.each([1, 3, 5])("isOdd should return true for %i", (a) => {
+  const result = isOdd(a);
+  expect(result).toBe(true);
+});
+
+it.each([
+  { value: 1, odd: true },
+  { value: 2, odd: false },
+  { value: 3, odd: true },
+])("isOdd should $odd true for $value", ({ value, odd }) => {
+  const result = isOdd(value);
+  expect(result).toBe(odd);
 });
